8c8da3238c07f7307ee5dda0ed205148fa0876c0
Frah01 <vinario.francesco@gmail.com>
2023-02-20 13:37:33 +0100

    Esercizio base completo

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic as Comic;
+
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    public function index()
+    {
+
+        $cards = Comic::all();
+
+        $menu = [
+
+            'Characters',
+            'Comics',
+            'Movies',
+            'TV',
+            'GAMES',
+            'Collectibles',
+            'Videos',
+            'Fans',
+            'News',
+            'Shop'
+        ];
+
+        $icons = [
+            [
+                'nome' => 'Digital Comics',
+                'img' => '../resources/img/buy-comics-digital-comics.png'
+            ],
+            [
+                'nome' => 'DC Merchandise',
+                'img' => '../resources/img/buy-comics-merchandise.png'
+            ],
+            [
+                'nome' => 'Subscription',
+                'img' => '../resources/img/buy-comics-subscriptions.png'
+            ],
+            [
+                'nome' => 'Comic Shop Locator',
+                'img' => '../resources/img/buy-comics-shop-locator.png'
+            ],
+            [
+                'nome' => 'CD Power Visa',
+                'img' => '../resources/img/buy-dc-power-visa.svg'
+            ]
+        ];
+
+        $socials = [
+            'facebook' => '../resources/img/footer-facebook.png',
+            'twitter' => '../resources/img/footer-twitter.png',
+            'youtube' => '../resources/img/footer-youtube.png',
+            'pinterest' => '../resources/img/footer-pinterest.png',
+            'periscope' => '../resources/img/footer-periscope.png',
+        ];
+        return view('home', compact('menu', 'socials', 'icons', 'cards'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController as ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,56 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-
-    $cards = config('comics');
-    $menu = [
-
-        'Characters',
-        'Comics',
-        'Movies',
-        'TV',
-        'GAMES',
-        'Collectibles',
-        'Videos',
-        'Fans',
-        'News',
-        'Shop'
-    ];
-
-    $icons = [
-        [
-            'nome' => 'Digital Comics',
-            'img' => '../resources/img/buy-comics-digital-comics.png'
-        ],
-        [
-            'nome' => 'DC Merchandise',
-            'img' => '../resources/img/buy-comics-merchandise.png'
-        ],
-        [
-            'nome' => 'Subscription',
-            'img' => '../resources/img/buy-comics-subscriptions.png'
-        ],
-        [
-            'nome' => 'Comic Shop Locator',
-            'img' => '../resources/img/buy-comics-shop-locator.png'
-        ],
-        [
-            'nome' => 'CD Power Visa',
-            'img' => '../resources/img/buy-dc-power-visa.svg'
-        ]
-    ];
-
-    $socials = [
-        'facebook' => '../resources/img/footer-facebook.png',
-        'twitter' => '../resources/img/footer-twitter.png',
-        'youtube' => '../resources/img/footer-youtube.png',
-        'pinterest' => '../resources/img/footer-pinterest.png',
-        'periscope' => '../resources/img/footer-periscope.png',
-    ];
-
-    return view('home', compact('cards', 'menu', 'icons', 'socials'));
-})->name('home');
+Route::get('/', [ComicController::class, 'index'])->name('homepage');
 
 Route::get('detail-comics/{id}', function ($id) {
     $menu = [
